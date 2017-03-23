e76d6696644d628dcbd4942accdfd9b78310c46b
Anthony Boccadoro <aboccadoro@umass.edu>
2017-03-23 14:49:37 -0400

    first compile

--- src/BoundedBuffer.cpp
+++ src/BoundedBuffer.cpp
@@ -2,19 +2,40 @@
 
 BoundedBuffer::BoundedBuffer(int N){
 	//TODO: constructor to initiliaze all the varibales declared in BoundedBuffer.h
+	buffer = new int[N];
+	buffer_size = N;
+	buffer_cnt = 0;
+	buffer_last = 0;
+	buffer_lock = PTHREAD_MUTEX_INITIALIZER;
+	buffer_full = PTHREAD_COND_INITIALIZER;
+	buffer_empty = PTHREAD_COND_INITIALIZER;
 }
 
 
 void BoundedBuffer::append(int data){
 	//TODO: append a data item to the circular buffer
+	pthread_mutex_lock(&buffer_lock);
+	if(buffer_cnt == buffer_size) pthread_cond_wait(&buffer_full, &buffer_lock);
+	buffer[buffer_size - 1 - buffer_cnt++] = buffer_last = data;
+	pthread_cond_broadcast(&buffer_empty);
+	pthread_mutex_unlock(&buffer_lock);
 }
 
 int BoundedBuffer::remove(){
 	//TODO: remove and return a data item from the circular buffer
-	return -1;
+	pthread_mutex_lock(&buffer_lock);
+	if(isEmpty()) pthread_cond_wait(&buffer_empty, &buffer_lock);
+	if(buffer_cnt == buffer_size) {
+		buffer[0] = 0;
+		pthread_cond_broadcast(&buffer_full);
+	}
+	else buffer[buffer_cnt - 1] = 0;
+	pthread_mutex_unlock(&buffer_lock);
+	buffer_cnt--;
+	return buffer_last;
 }
 
 bool BoundedBuffer::isEmpty(){
 	//TODO: check is the buffer is empty
-	return true;
+	return buffer_cnt == 0;
 }
--- src/ProducerConsumer.cpp
+++ src/ProducerConsumer.cpp
@@ -1,16 +1,59 @@
 #include "ProducerConsumer.h"
+#include <signal.h>
 
 //TODO: add BoundedBuffer, locks and any global variables here
+BoundedBuffer *buffer;
+int p_cnt = 0;
+int c_cnt = 0;
+int p_sleep;
+int c_sleep;
+int bound;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 void InitProducerConsumer(int p, int c, int psleep, int csleep, int items){
 	//TODO: constructor to initialize variables declared
 	//also see instruction for implementation
+	buffer = new BoundedBuffer(items);
+	for(int i = 0; i < p; i++) {
+		pthread_t tid;
+		pthread_create(&tid, NULL, producer, (void *)tid);
+	}
+	for(int i = 0; i < c; c++) {
+		pthread_t tid;
+		pthread_create(&tid, NULL, consumer, (void *)tid);
+	}
+	p_sleep = psleep;
+	c_sleep = csleep;
+	bound = items;
 }
 
 void* producer(void* threadID){
 	//TODO: producer thread, see instruction for implementation
+	while(true) {
+		if(p_cnt + c_cnt > bound) {
+			pthread_join((pthread_t)threadID, NULL);
+			pthread_detach((pthread_t)threadID);
+		}
+		sleep(p_sleep);
+		int data = rand() % 10 + 1;
+		pthread_mutex_lock(&lock);
+		buffer->append(data);
+		p_cnt++;
+		pthread_mutex_unlock(&lock);
+	}
 }
 
 void* consumer(void* threadID){
 	//TODO: consumer thread, see instruction for implementation
+	while(true) {
+		if(p_cnt + c_cnt > bound) {
+			pthread_join((pthread_t)threadID, NULL);
+			pthread_detach((pthread_t)threadID);
+		}
+		sleep(c_sleep);
+		pthread_mutex_lock(&lock);
+		buffer->remove();
+		c_cnt--;
+		pthread_mutex_unlock(&lock);
+	}
 }
